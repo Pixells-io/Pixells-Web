4807a999f3d570da584d4df2b0b65233d3cdf8d3
Dekepdevelop <dekepdevelop@gmail.com>
2024-12-02 17:55:33 -0600

    Update

--- src/components/Inputs/InputForm.jsx
+++ src/components/Inputs/InputForm.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InputForm({ type, name, placeholder }) {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      className="border[#D7D7D7] w-full rounded-xl border px-4 py-2 font-roboto text-sm text-grisHeading"
-      style={{ outline: "none" }}
-    />
+    <div className="relative w-full">
+      <input
+        id={name} // Añadir id que coincida con htmlFor del label
+        type={type}
+        name={name}
+        className="border[#D7D7D7] w-full rounded-xl border px-4 py-3 font-roboto text-sm text-grisHeading"
+        style={{ outline: "none" }}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <label
+        htmlFor={name} // Añadir htmlFor que coincida con el id del input
+        className={`absolute left-4 transition-all duration-200 ${
+          isFocused
+            ? "border[#D7D7D7] top-[-14px] text-xs text-grisHeading"
+            : "top-2 text-sm text-grisHeading"
+        }`}
+      >
+        {placeholder}
+      </label>
+    </div>
   );
 }
 
--- src/pages/Checkout/CheckoutForm.jsx
+++ src/pages/Checkout/CheckoutForm.jsx
@@ -1,26 +1,29 @@
 import InputForm from "@/components/Inputs/InputForm";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CheckoutForm() {
   return (
     <div className="flex min-h-screen items-center justify-center text-center align-middle">
-      <div className="w-1/5 items-center justify-center">
+      <div className="w-1/4 items-center justify-center">
         <div className="flex items-center justify-center py-6 text-center">
-          <img src="/img/logo.webp" width={"60px"} />
+          <Link to={"/"}>
+            <img src="/img/logo.webp" width={"60px"} />
+          </Link>
         </div>
-        <div className="py-4 text-left">
+        <div className="py-4 text-center">
           <span className="font-poppins text-2xl font-semibold text-grisHeading">
             Crear Cuenta Nueva
           </span>
         </div>
         <div className="text-left">
           <span className="font-poppins text-sm font-normal text-grisHeading">
-            Registrate y disfruta el sistema Pixells con <br /> un usuario de
-            forma gratuita. Haz que tu <br /> empresa sea profesional y con
-            procesos <br /> controlados.
+            Registrate y disfruta el sistema Pixells con un usuario de forma
+            gratuita. Haz que tu empresa sea profesional y con procesos
+            controlados.
           </span>
         </div>
-        <div className="mt-14 space-y-4">
+        <div className="mt-14 space-y-6">
           <InputForm
             type="email"
             name="email"
@@ -32,6 +35,59 @@ function CheckoutForm() {
             name="confirm_password"
             placeholder="Confirmar Contraseña"
           />
+        </div>
+        <div className="mt-6 flex gap-4 text-left">
+          <label
+            htmlFor="hr"
+            className="light:text-black flex flex-row items-center gap-2.5 dark:text-white"
+          >
+            <input
+              id="hr"
+              type="checkbox"
+              name="checkbox"
+              className="peer hidden"
+            />
+            <div className="light:bg-[#e8e8e8] flex h-5 w-5 rounded-md border border-[#a2a1a833] transition peer-checked:bg-primario dark:bg-[#212121]">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 12.6111L8.92308 17.5L20 6.5"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </div>
+          </label>
+          <span className="font-poppins text-xs text-grisHeading">
+            Acepto los{" "}
+            <Link className="text-blue-600" to={"/terminos-y-condiciones"}>
+              Términos y Condiciones
+            </Link>
+            ,{" "}
+            <Link className="text-blue-600" to={"/aviso-de-privacidad"}>
+              Aviso de Privacidad
+            </Link>{" "}
+            y el{" "}
+            <Link className="text-blue-600">Concentimiento Electrónico </Link>
+            para el uso de Pixells.
+          </span>
+        </div>
+        <div className="mt-12">
+          <button className="rounded-xl bg-primario px-12 py-3 font-poppins text-sm text-white hover:bg-primarioBotones">
+            CREAR
+          </button>
+        </div>
+        <div className="mt-6">
+          <span className="font-poppins text-xs text-grisHeading">
+            Si ya tienes una cuenta,{" "}
+            <Link className="text-blue-600">Inicia Sesión</Link>.
+          </span>
         </div>
       </div>
     </div>
