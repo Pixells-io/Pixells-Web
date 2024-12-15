22f265f6f63e733fdc3549586a33abd71440a965
Dekepdevelop <dekepdevelop@gmail.com>
2024-12-15 13:45:17 -0600

    Update InputTitle.jsx

--- src/components/Inputs/InputTitle.jsx
+++ src/components/Inputs/InputTitle.jsx
@@ -7,7 +7,7 @@ function InputTitle({ type, name, placeholder }) {
       <input
         type={type}
         name={name}
-        className="w-full rounded-lg border border-grisHeading px-4 py-2 font-roboto text-sm text-grisHeading"
+        className="w-full rounded-lg border border-grisHeading px-4 py-2 font-roboto text-base text-grisHeading"
       />
     </div>
   );
