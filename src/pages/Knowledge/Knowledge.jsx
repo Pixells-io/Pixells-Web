55f4f27209f93c281e5d911272a972c718b9ffa2
Dekepdevelop <dekepdevelop@gmail.com>
2025-01-30 09:42:01 -0600

    up

---
+++ src/pages/Knowledge/Knowledge.jsx
@@ -0,0 +1,13 @@
+import Footer from "@/layouts/Footer";
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <span>Hola</span>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/Knowledge/arrayData.js
@@ -0,0 +1,19 @@
+const arrayData = [
+  {
+    name: "Organizacion",
+    sub_modules: [
+      {
+        name: "Usuarios",
+        actions: [
+          {
+            title: "Consultar Usuarios",
+            Description: "Informacion Html",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Proyect Management",
+  },
+];
